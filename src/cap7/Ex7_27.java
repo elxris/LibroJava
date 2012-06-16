40f6c06da074db4f3f57a78c8c81808a5c8a7ff5
Christian <Christian@Christian-PC.(none)>
2012-06-15 19:47:50 -0500

    Terminado el ejercicio 7.27

--- src/cap7/Ex7_27.java
+++ src/cap7/Ex7_27.java
@@ -1,5 +1,41 @@
 package cap7;
 
 public class Ex7_27 {
-
+    boolean[] array = new boolean[1000];
+    
+    public static void main( String args[] ){
+        Ex7_27 ejercicio = new Ex7_27();
+        ejercicio.setearEnTrue();
+        ejercicio.paseo();
+        
+        System.out.print("Los numeros siguientes: ");
+        ejercicio.sobrevivientes();
+        System.out.print(". Son primos.");
+    }
+    
+    public void setearEnTrue(){
+        for(int i = 0; i < array.length ; i++){
+            array[i] = true;
+        }
+    }
+    
+    public void paseo(){
+        for(int i = 2; i < array.length; i++){
+            iteracion(i);
+        }
+    }
+    
+    public void iteracion(int n){
+        for(int i = 2; (i*n) < array.length; i++){
+            array[i*n] = false;
+        }
+    }
+    
+    public void sobrevivientes(){
+        for(int i = 2; i < array.length; i++){
+            if(array[i] == true){
+                System.out.printf("%d, ", i);
+            }
+        }
+    }
 }
